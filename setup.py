7069615d429e990e00ebab5951185e40c6b47356
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2022-02-07 16:20:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+#requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="Economy.py",
@@ -12,7 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ErrorThree/Python-Economy/",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[''],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
